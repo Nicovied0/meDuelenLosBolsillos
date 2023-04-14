9acf149cc3f68378a1489bb0e48865435f8da7ac
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-04-14 14:37:53 -0300

    new info and asicronc function

--- Component/Dolares.jsx
+++ Component/Dolares.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
-const Dolares = () => {
-  return <div>Dolares</div>;
-};
+export default function Dolares({ data }) {
+  return (
+    <div>
+      <div>
+        <h2>dolar oficial</h2>
+        <img></img>
+        <h3>compra</h3>
+        <h4>$300</h4>
+        <h3>Venta</h3>
+        <h4>$300</h4>
+      </div>
+      <h1>Lista de artículos</h1>
+      {/* {data.map(({ id, title, body }) => (
+        <div key={id}>
+          <h3>{title}</h3>
+          <p>{body}</p>
+        </div>
+      ))} */}
+      {console.log(data)}
+    </div>
+  );
+}
 
-export default Dolares;
+export async function getStaticProps() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
